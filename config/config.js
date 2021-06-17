dddc643faddb02b10057c3b895d23b1f9f05be74
Kumar Nikhil <kumarn.ug18.cs@nitp.ac.in>
2021-06-18 00:40:35 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 var config = {};
 
 config.twilio = {
-    authToken: 'cf372eed6656f8d0fd969859f92c6068',
-    accountSid: 'ACcf91ca7086eef8fd5c3caed0147c3f89'
+    authToken: 'cf372eed6656f8d0',
+    accountSid: 'ACcf91ca70'
 };
 
 config.mlab = {
-    username: 'testing.sih.web@gmail.com',
-    password: 'theteam@1234'
+    username: '',
+    password: ''
 };
 
 
